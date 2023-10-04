0dd790141aa9e363a8c311d1bc0d79c3727e4ef9
evan.baillieu <baillieu.evan@gmail.com>
2023-10-04 11:57:50 +0200

    feat(backend): add not found route and start server

--- back-end/server.js
+++ back-end/server.js
@@ -1,18 +1,57 @@
 const http = require('http')
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
 
-app.get('*', function(req, res) {
-    res.json({ 
-        code: 404,
-        response: "Page Not Found"
-    });
+let corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
 });
+
+
+//route
+require('./src/routes/auth.routes')(app);
+require('./src/routes/user.routes')(app);
 
 const server = http.createServer(app)
 
 server.listen(8001, () => {
     console.log("your server is start http://localhost:8001/");
 })
+
+const db = require("./src/models");
+const Role = db.role;
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}

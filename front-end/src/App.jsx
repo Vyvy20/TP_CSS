d3ec7d8bcc0af25e547d7f6329bf09ac661b94ce
evan.baillieu <baillieu.evan@gmail.com>
2023-10-05 13:16:47 +0200

    feat(frontend): add login page in frontend

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import TestRoute from "./TestRoute";
+import { InputLogin } from "./components/inputLogin/inputLogin";
+import { LoginPage } from "./view/Login/login";
 
 function App() {
   return (
-    <>
+    <main>
       <Routes>
-        <Route path="/connect" element={<TestRoute />} />
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/connect" element={<InputLogin type={"text"} placeHolder={"Identifiant"} id={"input_identifiant"} name={"identifiant"} onChange={() => {}} />} />
       </Routes>
-    </>
+    </main>
   );
 }
 
---
+++ front-end/src/view/Login/login.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { InputLogin } from "../../components/inputLogin/inputLogin";
+import { SubmitLoginButton } from "../../components/submitLoginButton/submitLoginButton";
+import logo from '../../assets/image/Logo.png'
+import "./login.css"
+
+export const LoginPage = () =>{
+    const [user, setUser] = useState({});
+
+    const loginOnChange = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        
+        setUser((state) => ({
+            ...state,
+            [name]: value 
+        }))
+    }
+
+    const submitLoginHandeler = (e) => {
+        e.preventDefault()
+        console.log(user)
+    }
+
+    return (
+            <form action="" onSubmit={submitLoginHandeler} className={"loginPage"}>
+                <div>
+                    <img src={logo} hieght={"105px"}alt="Logo de votre marque" />
+                </div>
+                <div>
+                    <InputLogin type={"text"} placeholder={"Identifiant"} id={"input_identifiant"} name={"identifiant"} onChange={loginOnChange} />
+                    <InputLogin type={"password"} placeholder={"Mot de passe"} id={"input_password"} name={"password"} onChange={loginOnChange} />
+                </div>
+                <SubmitLoginButton />
+            </form>
+    );
+}

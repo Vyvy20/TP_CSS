99166b168dc8b1543fad97989941bf576a7c674f
vidal vidal <yurisaop786@gmail.com>
2023-10-20 13:41:23 +0200

    feat: add attribute to user entity

--- back-end/src/models/user.model.js
+++ back-end/src/models/user.model.js
@@ -1,5 +1,9 @@
 module.exports = (sequelize, Sequelize) => {
     const User = sequelize.define("users", {
+        id: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+        },
         username: {
             type: Sequelize.STRING
         },
@@ -8,6 +12,9 @@ module.exports = (sequelize, Sequelize) => {
         },
         password: {
             type: Sequelize.STRING
+        },
+        role: {
+            type: Sequelize.INTEGER
         }
     });
 

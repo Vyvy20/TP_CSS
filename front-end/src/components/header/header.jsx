6cdf6c071919576474816577210eb60875a7eb18
laughan <ch.laughan@gmail.com>
2023-10-23 15:11:06 +0200

    feat(front-end):chnage button to Navlnik

--- front-end/src/components/header/header.jsx
+++ front-end/src/components/header/header.jsx
@@ -1,17 +1,20 @@
+import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../../assets/image/Logo.png";
 import Logout from "../../assets/svg/Logout.svg";
 import Return from "../../assets/svg/Return.svg";
 import "./header.sass";
 
 export function Header() {
+  const navigate = useNavigate();
+
   return (
     <header>
-      <button>
+      <NavLink onClick={() => navigate(-1)}>
         <img src={Return} alt="Return icon" />
-      </button>
+      </NavLink>
 
       <img src={logo} alt="Logo de votre marque" />
-      <button>
+      <button onClick={() => alert("Logout")}>
         <img src={Logout} alt="Logout icon" />
       </button>
     </header>

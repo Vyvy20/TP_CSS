27c0703cb9c08377ef28ab69da88fd0d86b0eaa8
laughan <ch.laughan@gmail.com>
2023-10-23 14:44:09 +0200

    feat(front-end):add header

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,6 +3,7 @@ import { LoginPage } from "./view/Login/login";
 import "./App.css";
 import { InterventionItem } from "./components/InterventionItem/interventionItem";
 import { ListItervention } from "./view/ListItervention/ListItervention";
+import { Header } from "./components/header/header";
 
 export function App() {
   return (
@@ -10,9 +11,12 @@ export function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/listIntervention" element={<ListItervention />} />
-        <Route path="/connect" element={<InterventionItem isEnable={true} textStatus={"je Part"} />} />
+        <Route
+          path="/connect"
+          element={<InterventionItem isEnable={true} textStatus={"je Part"} />}
+        />
+        <Route path="/test" element={<Header />} />
       </Routes>
     </main>
   );
 }
-
--- front-end/src/components/header/header.jsx
+++ front-end/src/components/header/header.jsx
@@ -1,6 +1,19 @@
-export function Header() {
+import logo from "../../assets/image/Logo.png";
+import Logout from "../../assets/svg/Logout.svg";
+import Return from "../../assets/svg/Return.svg";
+import "./header.sass";
 
-    return (
-        <></>
-    )
+export function Header() {
+  return (
+    <header>
+      <a>
+        <img src={Return} alt="Return icon" />
+      </a>
+
+      <img src={logo} alt="Logo de votre marque" />
+      <a>
+        <img src={Logout} alt="Logout icon" />
+      </a>
+    </header>
+  );
 }

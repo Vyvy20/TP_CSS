6ab092a0ac8d56341b40c8b579d2a746c5251ff4
Vyvy20 <c.dang2001@gmail.com>
2023-10-27 14:46:17 +0200

    feat(header): add classname for image

--- front-end/src/components/header/header.jsx
+++ front-end/src/components/header/header.jsx
@@ -2,7 +2,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../../assets/image/Logo.png";
 import Logout from "../../assets/svg/Logout.svg";
 import Return from "../../assets/svg/Return.svg";
-import "./header.sass";
+import "./header.scss";
 
 export function Header() {
   const navigate = useNavigate();
@@ -13,7 +13,7 @@ export function Header() {
         <img src={Return} alt="Return icon" />
       </NavLink>
 
-      <img src={logo} alt="Logo de votre marque" />
+      <img className="logo" src={logo} alt="Logo de votre marque" />
       <button onClick={() => alert("Logout")}>
         <img src={Logout} alt="Logout icon" />
       </button>
